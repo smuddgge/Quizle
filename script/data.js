d0e8548dd04f42a8257ed19261ed362070dfee43
smuddgge <68658429+smuddgge@users.noreply.github.com>
2023-02-02 23:11:41 +0000

    Added more questions

--- script/data.js
+++ script/data.js
@@ -12,6 +12,34 @@ const questions = {
         1: 'Yes',
         2: 'Im not sure',
         'answer': 1
+    },
+    2: {
+        'question': 'Who invented the barcode?',
+        0: 'Dave',
+        1: 'Richard Strauss',
+        2: 'Norman Joseph Woodland',
+        'answer': 2
+    },
+    3: {
+        'question': 'Where is the check digit located on a UPC barcode?',
+        0: 'Left',
+        1: 'Middle',
+        2: 'Right',
+        'answer': 2
+    },
+    4: {
+        'question': 'What is the check digit for the UPC barcode : 81120401234',
+        0: '2',
+        1: '4',
+        2: '8',
+        'answer': 1
+    },
+    5: {
+        'question': 'What is the check digit for the UPC barcode : 85626000279',
+        0: '3',
+        1: '5',
+        2: '7',
+        'answer': 0
     }
 }
 
